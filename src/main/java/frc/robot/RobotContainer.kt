1bf829b8af8c0b15b35e36b4a7512d7d08c2ecf7
John Joseph <jpothen8@gmail.com>
2023-11-02 09:16:56 -0400

    fix null pointer to controller by initializing controller before object init

--- src/main/java/frc/robot/RobotContainer.kt
+++ src/main/java/frc/robot/RobotContainer.kt
@@ -18,6 +18,33 @@ import frc.robot.subsystems.ExampleSubsystem
  * to the various subsystems in this container to pass into to commands. The commands can just
  * directly reference the (single instance of the) object.
  */
-object RobotContainer {
+object RobotContainer
+{
 
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    private val driverController = CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT)
+
+    init
+    {
+        configureBindings()
+        // Reference the Autos object so that it is initialized, placing the chooser on the dashboard
+        Autos
+    }
+
+    /**
+     * Use this method to define your `trigger->command` mappings. Triggers can be created via the
+     * [Trigger] constructor that takes a [BooleanSupplier][java.util.function.BooleanSupplier]
+     * with an arbitrary predicate, or via the named factories in [GenericHID][edu.wpi.first.wpilibj2.command.button.CommandGenericHID]
+     * subclasses such for [Xbox][CommandXboxController]/[PS4][edu.wpi.first.wpilibj2.command.button.CommandPS4Controller]
+     * controllers or [Flight joysticks][edu.wpi.first.wpilibj2.command.button.CommandJoystick].
+     */
+    private fun configureBindings()
+    {
+        // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+        Trigger { ExampleSubsystem.exampleCondition() }.onTrue(ExampleCommand())
+
+        // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+        // cancelling on release.
+        driverController.b().whileTrue(ExampleSubsystem.exampleMethodCommand())
+    }
 }
