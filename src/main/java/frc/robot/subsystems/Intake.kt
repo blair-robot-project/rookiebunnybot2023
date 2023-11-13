721d751c35f3b4e7b59de026019f0aea0b1fc07c
Michael Spears <mts18142027@gmail.com>
2023-11-13 16:29:52 -0500

    In Intake.kt removed accidental constructor on Intake object because Kotlin objects can't have constructors

--- src/main/java/frc/robot/subsystems/Intake.kt
+++ src/main/java/frc/robot/subsystems/Intake.kt
@@ -7,7 +7,7 @@ import frc.robot.subsystems.motor.WrappedMotor
 
 // By making a subsystem a Kotlin object, we ensure there is only ever one instance of it.
 // It also reduces the need to have reference variables for the subsystems to be passed around.
-object Intake() : SubsystemBase() {
+object Intake : SubsystemBase() {
     var motor: WrappedMotor
     var piston //Somehow make a piston
     fun createWrappedMotor(id: Int) {
